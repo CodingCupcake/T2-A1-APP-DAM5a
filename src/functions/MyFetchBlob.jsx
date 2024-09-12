5b4479afb628f7733b8988ab7c24bbbb0a8af680
CodingCupcake <brjezunigalo@ittepic.edu.mx>
2024-09-12 10:57:27 -0700

    commit

--- src/functions/MyFetchBlob.jsx
+++ src/functions/MyFetchBlob.jsx
@@ -1,26 +1,15 @@
 function MyFetchBlob() {
 
-    let img1 = document.getElementById("IdImg1");
-    let img2 = document.getElementById("IdImg2");
-    //FIC: Fetch con codigo de promesa simplificada.
-    fetch('images/MazingerZ-1.png')
+    fetch('src/images/MazingerZ-1.png')
     .then(resp => resp.blob())
     .then(image => {
-        console.log(image);
-        //FIC: instancia para imagen 1
-        var imgPath1 = URL.createObjectURL(image);
-        img1.src = imgPath1;
+        document.getElementById("IdImg1").src = URL.createObjectURL(image);
     });
-    //FIC: Fetch con codigo de promesa normal.
-    fetch('images/MazingerZ-2.png')
-    .then(resp => {
-        return resp.blob();
-    })
+
+    fetch('src/images/MazingerZ-2.png')
+    .then(resp => resp.blob())
     .then(image => {
-        console.log(image);
-        //FIC: instancia para imagen 2
-        var imgPath2 = URL.createObjectURL(image);
-        img2.src = imgPath2;
+        document.getElementById("IdImg2").src = URL.createObjectURL(image);
     });
 
     return(
